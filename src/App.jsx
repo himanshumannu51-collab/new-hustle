4849bb09ab02e43bdec3ed5953378569b3cfddf9
himanshumannu51-collab <himanshu.mannu51@gmail.com>
2025-11-05 09:43:51 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,32 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Pages
+import HomePage from "./pages/HomePage";
+import HustlesPage from "./pages/HustlesPage";
+import ToolsPage from "./pages/ToolsPage";
+import GuidesPage from "./pages/GuidesPage";
+import ContactPage from "./pages/ContactPage";
+
+// Components
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/hustles" element={<HustlesPage />} />
+        <Route path="/tools" element={<ToolsPage />} />
+        <Route path="/guides" element={<GuidesPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
 
