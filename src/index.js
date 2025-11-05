b45c85510be906f5542ad132895acd58374dba90
himanshumannu51-collab <himanshu.mannu51@gmail.com>
2025-11-05 10:23:21 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'; // make sure App.jsx is here
-import './index.css';     // optional, keep your styles
+import App from './App';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
