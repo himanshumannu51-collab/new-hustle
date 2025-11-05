0ff7d119664298408e075ef8c6f7706b75abd367
himanshumannu51-collab <himanshu.mannu51@gmail.com>
2025-11-05 09:38:30 +0530

    Update HustlesPage.jsx

--- src/pages/HustlesPage.jsx
+++ src/pages/HustlesPage.jsx
@@ -1,1 +1,16 @@
+import React from "react";
+import HustleCard from "../components/HustleCard";
+import { hustles } from "../data/hustles";
 
+const HustlesPage = () => {
+  return (
+    <div className="p-6 space-y-6">
+      <h1 className="text-4xl font-bold">All Hustles</h1>
+      <div className="grid md:grid-cols-3 gap-6">
+        {hustles.map(h => <HustleCard key={h.id} hustle={h} />)}
+      </div>
+    </div>
+  );
+};
+
+export default HustlesPage;
