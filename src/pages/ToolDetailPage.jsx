62d18701aa006e110c4e6190ff2f51487e5d4660
himanshumannu51-collab <himanshu.mannu51@gmail.com>
2025-11-05 09:38:47 +0530

    Update ToolDetailPage.jsx

--- src/pages/ToolDetailPage.jsx
+++ src/pages/ToolDetailPage.jsx
@@ -1,1 +1,17 @@
+import React from "react";
+import ToolCard from "../components/ToolCard";
+import { tools } from "../data/tools";
+
+const ToolsPage = () => {
+  return (
+    <div className="p-6 space-y-6">
+      <h1 className="text-4xl font-bold">All Tools</h1>
+      <div className="grid md:grid-cols-3 gap-6">
+        {tools.map(t => <ToolCard key={t.id} tool={t} />)}
+      </div>
+    </div>
+  );
+};
+
+export default ToolsPage;
 
